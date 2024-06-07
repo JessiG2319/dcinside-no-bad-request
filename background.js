d4eab86268b3e77ff4436a040a54f9d0b3538fb3
xisuthros <xisuthros@utnapisht.im>
2024-06-07 21:38:35 +0900

    initial commit

---
+++ background.js
@@ -0,0 +1,19 @@
+'use strict';
+
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	let now = Date.now();
+
+	browser.cookies.getAll({ domain: "dcinside.com" }).then((cookies) => {
+    let promises = [];
+    cookies.forEach((cookie) => {
+      if (!cookie.session && cookie.expirationDate * 1000 < now) {
+        promises.push(browser.cookies.remove({ name: cookie.name, url: ((cookie.secure) ? 'https://' : 'http://') + cookie.domain + cookie.path }));
+      }
+    });
+    return Promise.allSettled(promises);
+  }).then((values) => {
+		sendResponse(values);
+	});
+
+	return true;
+});
---
+++ content.js
@@ -0,0 +1,28 @@
+'use strict';
+
+browser.runtime.sendMessage(null).then((values) => {
+  if (values.some((value) => value.value === null || value.status == "rejected")) {
+    console.error("Something has gone wrong. Here are the details.");
+    console.group();
+    values.forEach((value) => {
+      if (value.value === null) {
+        console.error("reason: Unknown");
+      } else if (value.status == "rejected") {
+        console.error("reason: " + value.reason);
+      }
+    });
+    console.groupEnd();
+  } else if (values.length == 0) {
+		// Do nothing
+	} else if (values.length == 1) {
+		console.log('Removed a expired cookie.');
+    console.group();
+    values.forEach((value) => console.log(value.value));
+    console.groupEnd();
+	} else {
+		console.log('Removed ' + values.length + ' number of expired cookies.');
+    console.group();
+    values.forEach((value) => console.log(value.value));
+    console.groupEnd();
+	}
+});
